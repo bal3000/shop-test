f305cf0019dcfa6cb051d5c521388c2185bdf1de
bal3000 <bal3000286@googlemail.com>
2016-11-25 20:41:14 +0000

    more animation tests

--- src/app/components/productdetail/productdetail.component.ts
+++ src/app/components/productdetail/productdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, trigger, state, style, transition, animate  } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes, AnimationTransitionEvent  } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
@@ -13,16 +13,40 @@ import { BasketService } from '../../services/basketservice.service';
   styleUrls: ['./productdetail.component.css'],
   animations: [
     trigger('infoState', [
-      state('inactive', style({
-        opacity: 0,
-        maxHeight: '0px'
-      })),
-      state('active', style({
-        opacity: 1,
-        maxHeight: '100%'
-      })),
-      transition('inactive => active', animate('1000ms ease-in')),
-      transition('active => inactive', animate('1000ms ease-out'))
+      state('inactive', style({height: 0})),
+      state('active', style({height: '*'})),
+      transition('inactive => active', [
+        style({height: 0}),
+        animate('350ms ease-in', style({height: '*'}))
+      ]),
+      transition('active => inactive', animate('450ms ease-out'))
+    ]),
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        animate(450, keyframes([
+          style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+        ]))
+      ]),
+      transition('* => void', [
+        animate(450, keyframes([
+          style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+          style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+          style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+        ]))
+      ])
+    ]),
+    trigger('fadeIn', [
+      state('in', style({opacity: 1})),
+      transition('void => *', [
+        style({opacity: 0}),
+        animate('350ms ease-in')
+      ]),
+      transition('* => void', [
+        animate('350ms ease-out', style({opacity: 0}))
+      ])
     ])
   ]
 })
@@ -51,5 +75,9 @@ export class ProductDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  animationStarted(event: AnimationTransitionEvent): any {
+    console.log(event);
   }
 }
