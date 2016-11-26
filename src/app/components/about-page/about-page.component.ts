d6335671308958690f8b6dfa82b169432c01729e
bal3000 <bal3000286@googlemail.com>
2016-11-26 14:31:48 +0000

    route animations added

--- src/app/components/about-page/about-page.component.ts
+++ src/app/components/about-page/about-page.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, trigger, transition, animate, style, state } from '@angular/core';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.css']
+  styleUrls: ['./about-page.component.css'],
+  animations: [
+    trigger('routeAnimation', [
+      state('*',
+        style({
+          opacity: 1
+        })
+      ),
+      transition(':enter', [
+        style({
+          opacity: 0
+        }),
+        animate('1000ms ease-in')
+      ]),
+      transition(':leave', [
+        animate('100ms ease-out', style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class AboutPageComponent implements OnInit {
+  @HostBinding('@routeAnimation') get routeAnimation() {
+    return true;
+  }
 
+  @HostBinding('style.display') get display() {
+    return 'block';
+  }
   constructor() { }
 
   ngOnInit() {
